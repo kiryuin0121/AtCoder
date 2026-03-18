3b2f906c1be2c6378829ccbf24ea852488ab3313
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-03-18 12:30:25 +0900

    abc081-bをAC

--- abc081/b/abc081-b.ts
+++ abc081/b/abc081-b.ts
@@ -1,2 +1,15 @@
-const input:string = await Bun.stdin.text();
-const lines:string[] = input.trim().split("\n").map((line) => line.trim());
+const input: string = await Bun.stdin.text();
+const lines: string[] = input
+  .trim()
+  .split("\n")
+  .map((line) => line.trim());
+
+const N = Number(lines[0]);
+let numbers = lines[1]?.split(" ").map(Number) as number[];
+
+let count = 0;
+while(numbers.every(num=>num%2===0)){
+  numbers = numbers.map(num=>num/2);
+  count++;
+}
+console.log(count);
