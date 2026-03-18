645fe9fdeb10ffa1f4192dd147bef96bb3eae89b
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-03-18 22:18:38 +0900

    abc085-cをAC

--- abc085/c/abc085-c.ts
+++ abc085/c/abc085-c.ts
@@ -1,2 +1,20 @@
-const input:string = await Bun.stdin.text();
-const lines:string[] = input.trim().split("\n").map((line) => line.trim());
+const input: string = await Bun.stdin.text();
+const lines: string[] = input
+  .trim()
+  .split("\n")
+  .map((line) => line.trim());
+
+const [N, Y] = lines[0]?.split(" ").map(Number) as number[];
+let enable = false;
+loop: for (let a = 0; a <= N!; a++) {
+  for (let b = 0; b <= N! - a; b++) {
+    for (let c = 0; c <= N! - a - b; c++) {
+      if (a + b + c === N! && 10000 * a + 5000 * b + 1000 * c === Y!) {
+        enable = true;
+        console.log(a, b, c);
+        break loop;
+      }
+    }
+  }
+}
+if (!enable) console.log(-1, -1, -1);
