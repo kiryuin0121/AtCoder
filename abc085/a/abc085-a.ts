fe9d3910bb6b715fbcfe6a6eeddb2331a350bb21
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-03-21 00:03:45 +0900

    abc085-aをAC

--- abc085/a/abc085-a.ts
+++ abc085/a/abc085-a.ts
@@ -1,4 +1,4 @@
 const input:string = await Bun.stdin.text();
 const lines:string[] = input.trim().split("\n").map((line) => line.trim());
 const S = lines[0] as string;
-console.log(S.replace("2017/","2018/"))
+console.log(S.replace("2017/","2018/"));
