9cd8db233c584e1b77b0b3be3bc109fe53368f2e
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-03-20 23:29:51 +0900

    abc088-aをAC

--- abc088/a/abc088-a.ts
+++ abc088/a/abc088-a.ts
@@ -1,2 +1,6 @@
 const input:string = await Bun.stdin.text();
 const lines:string[] = input.trim().split("\n").map((line) => line.trim());
+const N= Number(lines[0]) as number;
+const A =Number(lines[1]) as number;
+console.log(N%500<=A?"Yes":"No");
+
