f8b3f4e4aeef680af5b19ef48def601828f0fb14
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-03-21 00:09:12 +0900

    abc069-bをAC

--- abc069/b/abc069-b.ts
+++ abc069/b/abc069-b.ts
@@ -1,2 +1,4 @@
 const input:string = await Bun.stdin.text();
 const lines:string[] = input.trim().split("\n").map((line) => line.trim());
+const s = lines[0] as string;
+console.log(`${s[0]}${s.length-2}${s[s.length-1]}`);
