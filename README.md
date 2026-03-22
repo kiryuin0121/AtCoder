8306c2cd25276f74380a8f242d06a78709356177
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-03-22 15:57:30 +0900

    abc082-bをAC

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 https://atcoder.jp/contests/abs
 
-https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-8-%E5%95%8F--abc-085-c---otoshidama-300-%E7%82%B9
+https://qiita.com/drken/items/fd4e5e3630d0f5859067#5-%E9%81%8E%E5%8E%BB%E5%95%8F%E7%B2%BE%E9%81%B8-10-%E5%95%8F
 
 ## AtCoderProblems
 
--- abc082/b/abc082-b.ts
+++ abc082/b/abc082-b.ts
@@ -1,2 +1,10 @@
 const input:string = await Bun.stdin.text();
 const lines:string[] = input.trim().split("\n").map((line) => line.trim());
+
+const s = lines[0] as string;
+const t = lines[1] as string;
+
+const minS = s.split("").sort((a,b)=>a.localeCompare(b)).join("");
+const maxT = t.split("").sort((a,b)=>b.localeCompare(a)).join("");
+
+console.log(minS<maxT?"Yes":"No");
