994beb2dd45400ca78c040c83db55fe4febd3f47
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-04-05 13:42:15 +0900

    abc095-bをAC

--- abc095/b/abc095-b.ts
+++ abc095/b/abc095-b.ts
@@ -1,2 +1,9 @@
 const input:string = await Bun.stdin.text();
 const lines:string[] = input.trim().split("\n").map((line) => line.trim());
+const [N,X]= lines[0]?.split(" ").map(Number) as [number,number];
+const mArr = lines.slice(1).map(Number);
+let restAmount = mArr.reduce((amount,m) => {
+  return amount-=m
+},X);
+const minM = Math.min(...mArr);
+console.log(N+Math.floor(restAmount/minM));
