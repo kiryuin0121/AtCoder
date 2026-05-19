0b4e2a81350edc32f887bfeb9cf9b0862582b13d
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-05-19 17:38:42 +0900

    abc169-aをAC

---
+++ abc169/a/abc169-a.ts
@@ -0,0 +1,4 @@
+const input:string = await Bun.stdin.text();
+const lines:string[] = input.trim().split("\n").map((line) => line.trim());
+const [A,B] = lines[0]?.split(" ").map(Number) as [number,number];
+console.log(A*B);
